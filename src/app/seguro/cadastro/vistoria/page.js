a4fe619f448e63ed9f30db3949d994fc7e934ce6
Laura Trujillo Rosal <lauratrugillo@gmail.com>
2023-11-07 15:34:51 -0300

    Vistoria
    criação da página de vistoria

---
+++ src/app/seguro/cadastro/vistoria/page.js
@@ -0,0 +1,18 @@
+import './Vistoria.scss'
+
+export default function Vistoria(){
+
+    return(
+        <main>
+            <div class='container4'>
+                <center>
+                    <h1 id='vistoria_computador'>Por favor, realize esta etapa utilizando um aparelho móvel.</h1>
+                    <h1 id='vistoria_movel'>Siga as intruções exibidas na tela</h1>
+                    <div class='container5'>
+                        <img src='../../camera.png' alt="" />
+                    </div>
+                </center>
+            </div>
+        </main>
+    )
+}
